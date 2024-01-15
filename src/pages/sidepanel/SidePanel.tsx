ae7997e45d8a7944af48fa3b668cc92c81f0f9ce
JunseoPark123 <pjunseo7965@gmail.com>
2024-01-15 14:17:22 +0900

    feat: add sidepanel logic draft

--- src/pages/sidepanel/SidePanel.tsx
+++ src/pages/sidepanel/SidePanel.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import React, { useState, useEffect } from 'react';
 import '@pages/sidepanel/SidePanel.css';
 import useStorage from '@src/shared/hooks/useStorage';
 import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage';
@@ -8,37 +7,35 @@ import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 
 const SidePanel = () => {
   const theme = useStorage(exampleThemeStorage);
+  const [capturedImages, setCapturedImages] = useState([]);
+
+  useEffect(() => {
+    const handleMessage = message => {
+      if (message.action === 'capturedImage') {
+        // 임시로 캡처된 이미지를 6개까지 저장합니다.
+        const newImages = [...capturedImages, message.imageUri];
+        if (newImages.length > 6) newImages.shift();
+        setCapturedImages(newImages);
+      }
+    };
+    chrome.runtime.onMessage.addListener(handleMessage);
+    return () => {
+      chrome.runtime.onMessage.removeListener(handleMessage);
+    };
+  }, [capturedImages]);
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: theme === 'light' ? '#fff' : '#000',
-      }}>
-      <header className="App-header" style={{ color: theme === 'light' ? '#000' : '#fff' }}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/sidepanel/SidePanel.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: theme === 'light' && '#0281dc', marginBottom: '10px' }}>
-          Learn React!
-        </a>
-        <button
-          style={{
-            backgroundColor: theme === 'light' ? '#fff' : '#000',
-            color: theme === 'light' ? '#000' : '#fff',
-          }}
-          onClick={exampleThemeStorage.toggle}>
-          Toggle theme
-        </button>
-      </header>
+    <div className="App" style={{ backgroundColor: theme === 'light' ? '#fff' : '#000' }}>
+      <div className="captured-images-container">
+        {capturedImages.map((imageUri, index) => (
+          <img key={index} src={imageUri} alt={`Captured ${index}`} />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default withErrorBoundary(withSuspense(SidePanel, <div> Loading ... </div>), <div> Error Occur </div>);
+export default withErrorBoundary(
+  withSuspense(SidePanel, <React.Fragment>Loading...</React.Fragment>),
+  <React.Fragment>Error Occur</React.Fragment>,
+);
